b80a75b13f3028409bef5d8a747b4147d02d9344
zikrifikri21 <iniakunku329@gmail.com>
2025-10-07 16:11:13 +0800

    fix bug: logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,14 +1,14 @@
-import AppLogoIcon from './app-logo-icon';
+import { Milk } from 'lucide-react';
 
 export default function AppLogo() {
     return (
         <>
             <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+                <Milk className="size-5 text-white dark:text-black"></Milk>
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
+                    SF Parfum
                 </span>
             </div>
         </>
